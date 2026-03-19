4fecfc0e38ed92ab11a265533ed90c2987ed9001
barmakon4ik <barmakon4ik@gmail.com>
2026-03-19 10:50:19 +0100

    DB восстановлена, начало заполнения данными

--- vedenika/settings.py
+++ vedenika/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = "vedenika.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "cats_db",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
