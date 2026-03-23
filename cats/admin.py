1ac7b63ccbc5a597a4c4b515ccf5e23b0e5d4f93
barmakon4ik <barmakon4ik@gmail.com>
2026-03-23 06:33:41 +0100

    Перенес БД на ЯедексДиск, но как настроить пути непонятно

--- cats/admin.py
+++ cats/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django import forms
-from django.core.exceptions import ValidationError
 from parler.admin import TranslatableAdmin
 
 from .models import *
--- vedenika/settings.py
+++ vedenika/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "vedenika.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "D:/Meine Daten/YandexDisk/MyDocu/Katze/db.sqlite3",
     }
 }
 
